eb520cccae1abf22d27fc58fab823ebc8717b950
Aishakabeer <145667157+Aishakabeer@users.noreply.github.com>
2023-12-07 16:14:12 +0530

    Add files via upload

---
+++ operator_overloading_comparison.py
@@ -0,0 +1,15 @@
+class A:
+ def __init__(self, a):
+    self.a =a
+ def __gt__(self,other):
+     if(self.a>other.a):
+         return True
+     else:
+        return False
+obj1=A(1)
+obj2=A(3)
+if(obj1>obj2):
+     print("object 2 is greater than object 1")
+else:
+     print("object 1 is greater than object 2")
+        
